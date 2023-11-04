2ab6d263ef2122718356b5ab5690b2882eb49d6d
Junior Dering <juniordering@hotmail.com>
2023-11-04 16:31:21 -0400

    feat: statusbar

--- App.tsx
+++ App.tsx
@@ -9,11 +9,17 @@ import {
 } from '@expo-google-fonts/roboto'
 
 import { Loading } from '@components/Loading'
+import { StatusBar } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Group /> : <Loading />}
     </ThemeProvider>
   )
