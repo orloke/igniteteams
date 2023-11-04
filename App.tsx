cd9d7fb7bdb0cff7b157306985927235f0fb32cb
Junior Dering <juniordering@hotmail.com>
2023-11-04 16:28:51 -0400

    feat: styling loading component

--- App.tsx
+++ App.tsx
@@ -2,10 +2,19 @@ import { Group } from '@screens/Groups'
 import theme from '@theme/index'
 import { ThemeProvider } from 'styled-components/native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { Loading } from '@components/Loading'
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
     <ThemeProvider theme={theme}>
-      <Group />
+      {fontsLoaded ? <Group /> : <Loading />}
     </ThemeProvider>
   )
 }
