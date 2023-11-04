956661a1402561825adfc35ed2c7a056bd7a91ec
Junior Dering <juniordering@hotmail.com>
2023-11-04 19:49:50 -0400

    feat: highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@components/Header'
 import { Container, Title } from './styles'
+import { Highlight } from '@components/Highlight'
 
 export const Group = () => {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subTitle="Jogue com a sua turma" />
     </Container>
   )
 }
