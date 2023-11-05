e4515b87b8a086f64e3bb7fa5cf90df3b7f80823
Junior Dering <juniordering@hotmail.com>
2023-11-05 08:38:28 -0400

    feat: list to class

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,20 @@ import { Header } from '@components/Header'
 import { Container, Title } from './styles'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
 export const Group = () => {
+  const [groups, setGroups] = useState(['Galera da Xgrow'])
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subTitle="Jogue com a sua turma" />
-      <GroupCard title='Galera da Xgrow'/>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({item}) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
