6a96a6539160329cce9fa7b63fa2e9d3a14897dc
Junior Dering <juniordering@hotmail.com>
2023-11-04 20:07:51 -0400

    feat: groupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@components/Header'
 import { Container, Title } from './styles'
 import { Highlight } from '@components/Highlight'
+import { GroupCard } from '@components/GroupCard'
 
 export const Group = () => {
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subTitle="Jogue com a sua turma" />
+      <GroupCard title='Galera da Xgrow'/>
     </Container>
   )
 }
