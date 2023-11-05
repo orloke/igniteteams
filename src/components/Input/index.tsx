d6fb1e4acc85518da474a419ad189966579f3dfe
Junior Dering <juniordering@hotmail.com>
2023-11-05 10:04:32 -0400

    feat: style input placeholder

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,10 @@
 import { TextInputProps } from 'react-native'
 import { Container } from './styles'
+import { useTheme } from 'styled-components/native'
 
 type Props = TextInputProps & {}
 
 export function Input({...rest}: TextInputProps) {
-  return <Container {...rest} />
+  const {COLORS} = useTheme()
+  return <Container placeholderTextColor={COLORS.GRAY_300}  {...rest} />
 }
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -18,7 +18,7 @@ export function NewGroup() {
           title="Nova turma"
           subTitle="Crie a turma para adicionar as pessoas"
         />
-        <Input />
+        <Input placeholder='Nome da turma'/>
         <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
