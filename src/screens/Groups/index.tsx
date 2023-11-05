04e613abf9ffb0e1c7a6c3b59ea944ad361dcf54
Junior Dering <juniordering@hotmail.com>
2023-11-05 08:56:42 -0400

    feat: empty list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,11 @@ import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
+import { ListEmpty } from '@components/ListEmpty'
 
 export const Group = () => {
-  const [groups, setGroups] = useState(['Galera da Xgrow'])
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header />
@@ -14,7 +16,11 @@ export const Group = () => {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({item}) => <GroupCard title={item} />}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups?.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   )
